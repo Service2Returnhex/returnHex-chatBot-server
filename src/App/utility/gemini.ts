962c4f5fc2a2bdb58924911754c4a2854ce12995
Fahim550 <fahimrah1012023@gmail.com>
2025-07-15 17:22:04 +0600

    come error message

--- src/App/utility/gemini.ts
+++ src/App/utility/gemini.ts
@@ -1,20 +1,15 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  apiKey: process.env.GOOGLE_GEMINI_API_KEY,
-  //   project: process.env.GOOGLE_CLOUD_PROJECT,
-  vertexai: true,
+  //   apiKey: process.env.GOOGLE_GEMINI_API_KEY,
+  //   //   project: process.env.GOOGLE_CLOUD_PROJECT,
+  //   vertexai: true,
 });
 
 export async function generateGeminiReply(prompt: string): Promise<string> {
-  try {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt,
-    });
-    return response.text || "Sorry, I couldn't generate a response right now.";
-  } catch (error) {
-    console.error("Error generating content:", error);
-    return "An error occurred while generating the response.";
-  }
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+  return response.text || "Sorry, I couldn't generate a response right now.";
 }
