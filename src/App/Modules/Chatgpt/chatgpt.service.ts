c1632a4dc9ab228f09f81dca2bacbb89ad67d81e
Fahim550 <fahimrah1012023@gmail.com>
2025-08-02 22:22:11 +0600

    hotfix:chatgpt model change

--- src/App/Modules/Chatgpt/chatgpt.service.ts
+++ src/App/Modules/Chatgpt/chatgpt.service.ts
@@ -1,10 +1,10 @@
 import OpenAI from "openai";
 import { ChatCompletionMessageParam } from "openai/resources/index";
-import { ChatHistory } from "./chat-history.model";
-import { ShopInfo } from "../Page/shopInfo.model";
 import { Product } from "../Page/product.mode";
-import { CommentHistory } from "./comment-histroy.model";
 import { makePromtComment, makePromtDM } from "../Page/shop.promt";
+import { ShopInfo } from "../Page/shopInfo.model";
+import { ChatHistory } from "./chat-history.model";
+import { CommentHistory } from "./comment-histroy.model";
 
 const getResponseDM = async (
   senderId: string,
@@ -17,10 +17,10 @@ const getResponseDM = async (
     userHistoryDoc = new ChatHistory({ senderId, messages: [] });
   userHistoryDoc.messages.push({ role: "user", content: prompt });
 
-  const shop = await ShopInfo.findOne({shopId});
+  const shop = await ShopInfo.findOne({ shopId });
   if (!shop) throw new Error("Shop not found");
 
-  const products = await Product.find({shopId});
+  const products = await Product.find({ shopId });
 
   //save the post info. to the local database
   // create a script for run makePromtDM
@@ -34,7 +34,7 @@ const getResponseDM = async (
 
   const messages: ChatCompletionMessageParam[] = [
     { role: "system", content: getPromt },
-    { role: 'user', content: prompt}
+    { role: "user", content: prompt },
   ];
 
   const openai = new OpenAI({
@@ -42,7 +42,7 @@ const getResponseDM = async (
   });
 
   const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-4.1-nano",
     messages,
   });
   //replay should be in 20 token
@@ -53,7 +53,7 @@ const getResponseDM = async (
   userHistoryDoc.messages.push({ role: "assistant", content: reply });
   await userHistoryDoc.save();
 
-  //nlp: if same related question mathces with db, it will replay from the previous stored response. 
+  //nlp: if same related question mathces with db, it will replay from the previous stored response.
   return reply;
 };
 
@@ -79,35 +79,41 @@ export const getCommnetResponse = async (
       userName,
       messages: [],
     });
-  userCommnetHistoryDoc.messages.push({ commentId, role: "user", content: message });
+  userCommnetHistoryDoc.messages.push({
+    commentId,
+    role: "user",
+    content: message,
+  });
 
-  const shop = await ShopInfo.findOne({shopId});
+  const shop = await ShopInfo.findOne({ shopId });
   if (!shop) throw new Error("Shop not found");
 
-  const products = await Product.find({shopId});
+  const products = await Product.find({ shopId });
   const specificProduct = await Product.findOne({ shopId, postId });
 
   const getPrompt = makePromtComment(shop, products, specificProduct);
   const messages: ChatCompletionMessageParam[] = [
     { role: "system", content: getPrompt },
-    { role: "user", content: message}
+    { role: "user", content: message },
   ];
 
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-4.1-nano",
     messages,
   });
   const reply = `@[${commenterId}] ` + completion.choices[0].message.content;
 
-  userCommnetHistoryDoc.messages.push({commentId, role: "assistant", content: reply });
+  userCommnetHistoryDoc.messages.push({
+    commentId,
+    role: "assistant",
+    content: reply,
+  });
   await userCommnetHistoryDoc.save();
   return reply;
 };
-
-
 
 export const ChatgptService = {
   getResponseDM,
